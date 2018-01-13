6b7cee05b03c93fc32b70ddce2f4f727029e4f01
rishavanand <rishavdipsite@gmail.com>
2018-01-14 00:15:37 +0530

    Added thenextweb rss scraper

--- rss_thenextweb.py
+++ rss_thenextweb.py
@@ -2,18 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-def get_fossbyte_feed():
+def get_thenextweb_feed():
 
 	"""
-	Exatracts all news from FossByte's rss feed
+	Exatracts all news from TheNextWeb's rss feed
 	including their title, link, date and categories
 	"""
 
 	# Fetch feed
-	url = 'https://fossbytes.com/feed/'
+	url = 'http://feeds2.feedburner.com/thenextweb'
 	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
 	html = requests.get(url, headers=headers)
-	
+
 	# Extract news
 	soup = BeautifulSoup(html.text, 'xml')
 	items = soup.findAll('item')
@@ -23,17 +23,18 @@ def get_fossbyte_feed():
 		feed['title'] = item.find('title').text
 		feed['link'] = item.find('link').text
 		feed['date'] = item.find('pubDate').text
+		feed['poster'] = item.find('enclosure')['url']
 		feed['category'] = item.findAll('category')
 		cats = []
 		for category in feed['category']:
 			cats.append(category.text)
 		feed['category'] = cats
 		feeds[number] = feed
-
+	
 	return feeds
 
 # Save feed to file
-feeds = get_fossbyte_feed()
-my_file = open('rss_fossbytes.json', 'w')
+feeds = get_thenextweb_feed()
+my_file = open('rss_thenextweb.json', 'w')
 my_file.write(json.dumps(feeds))
 my_file.close()
